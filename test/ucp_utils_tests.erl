c7fde14640267ab34477a451a67b3181e18146de
systra <andrzejt@systra.com.pl>
2012-01-03 16:35:20 +0100

    Add new hex string conversion tests

--- test/ucp_utils_tests.erl
+++ test/ucp_utils_tests.erl
@@ -10,6 +10,10 @@ to_hex_test_() ->
     end},
    {"list to hexstr",
     ?_test(?assertEqual("081E", ucp_utils:to_hexstr([8,30])))},
+   {"binary to hexstr",
+    ?_test(?assertEqual("1E12", ucp_utils:to_hexstr(<<30,18>>)))},
+   {"unicode to hexstr",
+       ?_test(?assertEqual("001E01420105006B0061", ucp_utils:to_hexstr([30,322,261,107,97])))},
    {"str to hexstr",
     ?_test(?assertEqual("74657374", ucp_utils:to_hexstr("test")))}].
 
